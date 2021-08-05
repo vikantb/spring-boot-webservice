bfd57c690a73c835350a678d547ea2aa24b476a2
Vikant Bhati <vikant.bhati@nagarro.com>
2021-08-05 18:46:07 +0530

    change the package name of java test

--- src/test/java/com/springbootAssignment/webservice/SpringBootAssignmentApplicationTests.java
+++ src/test/java/com/springbootAssignment/webservice/SpringBootAssignmentApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nagarro.springBootAssignment;
+package com.springbootAssignment.webservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
